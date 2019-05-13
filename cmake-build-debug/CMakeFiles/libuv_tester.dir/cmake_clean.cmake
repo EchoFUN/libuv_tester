eb774a685e3fc83696a8074f91c8dd728b342d07
xukai <xukai.ken@bytedance.com>
2019-05-13 17:17:05 +0800

    Init

---
+++ cmake-build-debug/CMakeFiles/libuv_tester.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/libuv_tester.dir/main.c.o"
+  "libuv_tester.pdb"
+  "libuv_tester"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/libuv_tester.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
